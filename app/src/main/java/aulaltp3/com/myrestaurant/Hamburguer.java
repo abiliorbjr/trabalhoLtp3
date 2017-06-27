12dbceddf1659a03492084f23d2d1e6cc72f7a58
abiliorbjr <abiliorbjr@gmail.com>
2017-06-27 20:16:18 -0300

    acrescentamos tela resultado

--- app/src/main/java/aulaltp3/com/myrestaurant/Hamburguer.java
+++ app/src/main/java/aulaltp3/com/myrestaurant/Hamburguer.java
@@ -1,5 +1,6 @@
 package aulaltp3.com.myrestaurant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -26,7 +27,7 @@ public class Hamburguer extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_hamburgue);
-
+.
         hamburguerTriplo = (ImageButton) findViewById(R.id.hamburguerTriplo);
         tvQtdtripo       = (TextView) findViewById(R.id.qtdTripo);
 
@@ -55,3 +56,15 @@ public class Hamburguer extends AppCompatActivity {
 
     }
 }
+
+
+    public void onClick (View v)
+    {
+        Intent it = new Intent(this,Resultado1);
+
+        Intent it = new Intent(this,Resultado1.class)
+            it.putExtra("Nome", tvQtdtripo.getText().toString());
+        startActivity(it);
+
+
+    }
--- app/src/main/java/aulaltp3/com/myrestaurant/Resultado1.java
+++ app/src/main/java/aulaltp3/com/myrestaurant/Resultado1.java
@@ -3,34 +3,32 @@ package aulaltp3.com.myrestaurant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
-import android.view.View;
-import aulaltp3.com.myrestaurant.R;
+import android.widget.EditText;
+import android.view.*;
+import android.widget.*;
 
 public class Resultado1 extends AppCompatActivity {
 
     private TextView nome;
     private TextView quantidade;
-
+    private Button btnFechar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_Resultado1);
 
-        nome     = (TextView) findViewById(R.id.nome);
-        quantidade = (TextView) findViewById(R.id.quantidade);
+        nome     = (EditText) findViewById(R.id.nome);
+        quantidade = (EditText) findViewById(R.id.quantidade);
 
 
         String nome     = getIntent().getStringExtra("nome");
         String quantidade = getIntent().getStringExtra("quantidade");
 
 
-        nome.setText(nome);
-        quantidade.setText(quantidade);
 
 
-        btfechar.setOnClickListener(new View.OnClickListener() {
+        btnFechar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
