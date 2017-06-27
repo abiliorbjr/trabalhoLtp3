5058ff0b30c51dcb0bab1231a8ecdb5a181fe152
abiliorbjr <abiliorbjr@gmail.com>
2017-06-27 11:09:58 -0300

    botao hamburgues

--- app/src/main/java/aulaltp3/com/myrestaurant/Hamburguer.java
+++ app/src/main/java/aulaltp3/com/myrestaurant/Hamburguer.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import aulaltp3.com.myrestaurant.Hamburguer;
+
 
 /**
  * Created by AbilioJunior on 30/05/2017.
@@ -16,6 +19,7 @@ public class Hamburguer extends AppCompatActivity {
     private ImageButton hamburguerTriplo;
     private int qtpTripo;
     private TextView tvQtdtripo;
+    private Button btnCadastrarHamburguer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +34,15 @@ public class Hamburguer extends AppCompatActivity {
             public void onClick(View v) {
                 qtpTripo++;
                 tvQtdtripo.setText("Total= "+qtpTripo);
+
+
+         btnCadastrarHamburguer =(Button) findViewById(R.id.btnCadastrarHamburguer);
+                btnCadastrarHamburguer.setOnClickListener (new View.OnClickListener(){
+                    public void onClick(View v){
+                        
+                    }
+                }
+
             }
         });
 
